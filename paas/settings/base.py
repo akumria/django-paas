40e050e793621744db019769892c3f91a6ad3bfb
Anand Kumria <akumria@acm.org>
2012-04-05 18:55:07 +0100

    Add the default set of TEMPLATE_CONTEXT_PROCESSORS

--- paas/settings/base.py
+++ paas/settings/base.py
@@ -118,6 +118,18 @@ ROOT_URLCONF = '%s.urls' % PROJECT
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = '%s.wsgi.application' % PROJECT
 
+
+TEMPLATE_CONETCT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
+)
+
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
